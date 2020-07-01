15ad7f8b3fb2315aaca829f8b18db8cdc69dea53
Munasco <40294447+Munasco@users.noreply.github.com>
2020-07-01 19:13:12 -0400

    Committing boiler plate to master for now

---
+++ frontend/src/containers/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Helmet} from 'react-helmet'
+
+function Home() {
+
+    return (
+        <div>
+            <Helmet>
+                <title>Havenfound</title>
+                <meta name='description' content='login page'/>
+            </Helmet>
+            Home
+        </div>
+    )
+}
+
+export default Home
